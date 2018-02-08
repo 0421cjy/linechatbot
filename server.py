dad57398b01655661d0268d912835f5d41d0f2c6
0421cjy <0421cjy@naver.com>
2018-02-08 16:34:58 +0900

    line info setting

--- server.py
+++ server.py
@@ -30,8 +30,8 @@ from linebot.models import (
 from linebot.utils import PY3
 
 # get channel_secret and channel_access_token from your environment variable
-channel_secret = os.getenv('LINE_CHANNEL_SECRET', None)
-channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
+channel_secret = os.getenv('bb9ec7fc7536043f26e842eb073bf1de', None)
+channel_access_token = os.getenv('iWxmWeybGKxC1yddrgluw/c9glJN4RZBtFxecmz5e38sodmKKrqwd0vFA1qy9NMzUukFDPQsLWZKBjZVtYEdxfKOZldwcSDJpHBAZQXBVb0ESxySMOHUdhOFcDu+x/j8ph6O/nDEfFfOYMkq2JLeQQdB04t89/1O/w1cDnyilFU=', None)
 if channel_secret is None:
     print('Specify LINE_CHANNEL_SECRET as environment variable.')
     sys.exit(1)
